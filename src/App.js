96e96534eb92a21d138538c7c77a005bc0f242d3
Mellef <mellef_med_ali@hotmail.fr>
2022-10-08 00:17:42 +0100

    adding clearInterval method

--- src/App.js
+++ src/App.js
@@ -7,20 +7,25 @@ class App extends React.Component {
         imgSrc: "./logo.bmp",
         profession: "mechanical engineering",
         isShown: false,
-        delay: 0
+        delay: 0,
+        msg:""
     }
 
     componentDidMount() {
-        this.myInterval = setInterval(()=>{
+        this.myInterval =setInterval(()=>{
             this.setState({
                 delay: this.state.delay + 1
             })
         },1000)
     }
+    componentDidUpdate(){
+        this.state.delay>=10?(
+            clearInterval(this.myInterval)
+            ):(<></>)
+    }
 
     render() {
         const {delay} = this.state;
-        console.log("this.state.delay: ", {delay})
         const handleShow = () => {
             this.state.isShown ? (this.setState({ isShown: false })) : (this.setState({ isShown: true }));
         }
@@ -33,7 +38,9 @@ class App extends React.Component {
         )
         return (
             <>
-                {delay<1?(<></>):(<h1>This App component did mount since : {delay} secondes</h1>)}
+                {delay<1?(<></>):(<div><h3>This App component did mount since : {delay} secondes.</h3>
+                    <p style={{color:"red"}}>The counter will stop voluntarily after {10-delay}</p>
+                </div>)}
                 <button onClick={handleShow}>{this.state.isShown ? ("Hide") : ("Show")} Profile</button>
                 {this.state.isShown ? (
                     <>
@@ -51,5 +58,5 @@ export default App
 // OK Transform the App.js into a class-based component.
 // OK Implement a state for this class containing a Person ={ fullName,bio, imgSrc, profession} and a boolean shows.
 // OK Add a button that toggles the show state. When the show state is true, the person's profile will appear.
-// Create a field that shows the time interval since the last component was mounted using the component lifecycle.
+// OK Create a field that shows the time interval since the last component was mounted using the component lifecycle.
 // Hint: use the setInterval method.
